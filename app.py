2acd8f68e9a4f99a2b297edcef6be4676eaf6a6f
Stefano Soro <stefano@safetech.inf.br>
2020-05-11 00:00:19 -0300

    :recycle: Refactoring code

--- app.py
+++ app.py
@@ -2,21 +2,21 @@ from pytube import YouTube
 from pprint import pprint
 
 # Link do video
-yt = YouTube('https://www.youtube.com/watch?v=QYUXRpST1P0')
+yt = YouTube('https://www.youtube.com/watch?v=c_8Xqhl5XFA')
 
 # Debugar opções de download disponíveis
 streams = yt.streams
 streamToList = lambda x: list(x) if x else '\nErro ao obter stream\n'
-# pprint(streamToList(streams))
+pprint(streamToList(streams))
 
 # Filtrando qual stream quero baixar
-streams = streams.filter(progressive=True , mime_type='video/mp4', resolution="720p", )
+streams = streams.filter(progressive=True, mime_type='video/mp4', resolution="720p").order_by('itag').desc()
 print('\n\nStreams filtrados:')
 pprint(streamToList(streams))
 
 # Pegando primeiro da lista
 stream = streams.first()
-print('\n\nStreams escolhido: \n', stream)
+print('\n\nStream escolhido: \n', stream)
 
 # Faz download na pasta destino indicada
 stream.download('./videos')
--- readme.md
+++ readme.md
@@ -1,8 +1,13 @@
 # Script for download of movie video of YouTube
 
 ## commands to run
+
 ```
   pipenv --python 3.8
   pipenv install
   python app.py
 ```
+
+## Dicas para entender os filtros do youtube
+
+https://gist.github.com/sidneys/7095afe4da4ae58694d128b1034e01e2
